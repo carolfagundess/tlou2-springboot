ecd473da090339922b56b5caef453f77828ea7c3
Carol Fagundes <carolinarftk@gmail.com>
2025-05-19 20:53:55 -0300

    projeto criado

---
+++ src/test/java/com/devsuperior/tlou2/Tlou2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tlou2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tlou2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
